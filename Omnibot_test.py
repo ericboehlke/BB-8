a991d6cc8df59dc819682ea8cc76c4b119107740
root <root@spike.(none)>
2016-08-06 07:51:02 +0000

    fine control of tilt

--- Omnibot_test.py
+++ Omnibot_test.py
@@ -2,23 +2,26 @@ import time
 import math
 from Omnibot import * 
 
-zeroAngle = math.radians(180)
+zeroAngle = math.radians(103)
 
 twitch = Omnibot()
-# twitch.move(0, 40)
+for direction in range(0, 360, 10):
+    twitch.move(math.radians(direction), 5)
+    time.sleep(.5)
+    print math.radians(direction)
 
-# time.sleep(.25)
+time.sleep(.25)
 
-# # twitch.spin(40)
+twitch.spin(40)
 
-# time.sleep(2)
+time.sleep(.5)
 
-#twitch.spin(10)
+twitch.spin(0)
 
 time.sleep(2)
 
-# twitch.spin(-10)
+twitch.spin(-10)
 
-# time.sleep(2)
+time.sleep(2)
 
 twitch.stop()
